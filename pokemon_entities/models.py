b05e301a9d54aedbb5ec80100059b7aa231e77a9
RobinLoksli92 <gleb11tiun@mail.ru>
2022-03-28 20:35:00 +0200

    add verbose_name to models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,29 +4,28 @@ from django.conf import settings
 
 
 class Pokemon(models.Model):
-    title_ru = models.CharField(max_length=200, blank=True)
-    title_en = models.CharField(max_length=200, blank=True)
-    title_jp = models.CharField(max_length=200, blank=True)
-    image = models.ImageField(upload_to=settings.MEDIA_ROOT, null=True, blank=True)
-    appeared_at = models.DateTimeField(null=True, blank=True)
-    disappeared_at = models.DateTimeField(null=True, blank=True)
-    level = models.IntegerField(null=True, blank=True)
-    health = models.IntegerField(null=True, blank=True)
-    strength = models.IntegerField(null=True, blank=True)
-    defence = models.IntegerField(null=True, blank=True)
-    stamina = models.IntegerField(null=True, blank=True)
-    discription = models.TextField(null=True, blank=True)
-    evolution_from = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='evolution_to')
+    title_ru = models.CharField('Название покемона рус.',max_length=200, blank=True)
+    title_en = models.CharField('Название покемона англ.',max_length=200, blank=True)
+    title_jp = models.CharField('Название покемона яп.',max_length=200, blank=True)
+    image = models.ImageField('Картинка покемона',upload_to=settings.MEDIA_ROOT, null=True, blank=True)
+    discription = models.TextField('Описание',null=True, blank=True)
+    evolution_from = models.ForeignKey('self', verbose_name='Эволюционировал из', on_delete=models.CASCADE, null=True, blank=True, related_name='evolution_to')
 
     def __str__(self):
         return '{}'.format(self.title_ru)
 
 
 class PokemonEntity(models.Model):
-    lat = models.FloatField(blank=True)
-    long = models.FloatField(blank=True)
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, default='1')
-    
+    lat = models.FloatField('Широта', blank=True)
+    long = models.FloatField('Долгота',blank=True)
+    pokemon = models.ForeignKey(Pokemon, verbose_name='Эволюционировал из', on_delete=models.CASCADE, default='1')
+    appeared_at = models.DateTimeField('Появится в', null=True, blank=True)
+    disappeared_at = models.DateTimeField('Исчезнет в', null=True, blank=True)
+    level = models.IntegerField('Уровень', null=True, blank=True)
+    health = models.IntegerField('Здоровье',null=True, blank=True)
+    strength = models.IntegerField('Сила',null=True, blank=True)
+    defence = models.IntegerField('Защита',null=True, blank=True)
+    stamina = models.IntegerField('Выносливость',null=True, blank=True)
 
     def __str__(self):
         return '{}'.format(self.pokemon)
