978225584e288980217e208e692701565bde7893
RobinLoksli92 <gleb11tiun@mail.ru>
2022-03-31 15:36:09 +0200

    simplify code in show_all_pokemons

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -49,19 +49,13 @@ def show_all_pokemons(request):
 
     pokemons = Pokemon.objects.all()
     for pokemon in pokemons:
-        if pokemon.image:
-            pokemons_on_page.append({
-                'pokemon_id':pokemon.id,
-                'img_url': request.build_absolute_uri(pokemon.image.url),
-                'title_ru': pokemon.title_ru
-            })
-        else:
-            pokemons_on_page.append({
-                'pokemon_id':pokemon.id,
-                'title_ru': pokemon.title_ru
-            }) 
+        image_url = request.build_absolute_uri(pokemon.image.url) if pokemon.image else None
+        pokemons_on_page.append({
+            'pokemon_id':pokemon.id,
+            'img_url': image_url,
+            'title_ru': pokemon.title_ru
+        })
     
-
     return render(request, 'mainpage.html', context={
         'map': folium_map._repr_html_(),
         'pokemons': pokemons_on_page,
